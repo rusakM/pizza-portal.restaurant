91e80d64bdb9a343e22a9d1ff707d172a821440c
Mateusz Rusak <rusakkk@live.com>
2022-01-13 23:06:36 +0100

    remove statistics card in main page

--- src/components/landing-page/landing-page.component.jsx
+++ src/components/landing-page/landing-page.component.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlusCircle,
-  faPizzaSlice,
-  faChartLine,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlusCircle, faPizzaSlice } from "@fortawesome/free-solid-svg-icons";
 
 import MainMenuCard from "../main-menu-card/main-menu-card.component";
 
@@ -18,17 +14,11 @@ const LandingPage = ({ history }) => {
           </span>
           <p>Stwórz zamówienie</p>
         </MainMenuCard>
-        <MainMenuCard open={() => history.push("/orders/new")}>
+        <MainMenuCard open={() => history.push("/orders")}>
           <span className="font-icon">
             <FontAwesomeIcon icon={faPizzaSlice} />
           </span>
           <p>Nowe zamówienia</p>
-        </MainMenuCard>
-        <MainMenuCard open={() => history.push("/statistics")}>
-          <span className="font-icon">
-            <FontAwesomeIcon icon={faChartLine} />
-          </span>
-          <p>Statystyki</p>
         </MainMenuCard>
       </div>
     </div>
